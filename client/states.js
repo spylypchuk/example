621b41cd35c8a3348eccecccb29f6313575ed2a8
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-28 09:39:43 +0100

    Modified 4 items, New 4 items

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.postscool', {
+        views: {
+            app: {
+                controller: 'app_postscool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.postscool');
+                }
+            }
+        }
     });
 });
