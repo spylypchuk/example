c37a95f0032fd44fe0bf2f67b52e1d24ba8bd17f
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-28 14:25:17 +0100

    Modified 2 items, Deleted 2 items, New 2 items

--- client/mocks/checktest.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.checktest = window.mocks.checktest || {};
---
+++ client/mocks/sptest.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.sptest = window.mocks.sptest || {};
