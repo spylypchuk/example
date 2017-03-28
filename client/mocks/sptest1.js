add940a18cc888ab66dee881d6c88dc4a5de0fba
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-28 15:34:16 +0100

    Modified 2 items, Deleted 2 items, New 2 items

--- client/mocks/sptest1.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.sptest1 = window.mocks.sptest1 || {};
---
+++ client/mocks/sptest333.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.sptest333 = window.mocks.sptest333 || {};
