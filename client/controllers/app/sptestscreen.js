fbbcfe6492c534cc8ccf84ce06a9378c3acfb470
spylypchuksj <admin@studio.dev.powwowinc.net>
2017-03-28 14:19:28 +0100

    Modified 4 items, New 4 items

---
+++ client/controllers/app/sptestscreen.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_sptestscreen', app_sptestscreen);
+function app_sptestscreen($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.sptestscreen', {
+        views: {
+            app: {
+                controller: 'app_sptestscreen',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.sptestscreen');
+                }
+            }
+        }
     });
 });
